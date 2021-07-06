aade88ab034a8274adb0b90c2138c3798d97149d
ruben <fopoar@gmail.com>
2021-07-06 00:05:03 -0500

    ̂Add create/edit actions

--- src/Controller/RadioController.php
+++ src/Controller/RadioController.php
@@ -4,17 +4,26 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\AdvertisementRepository;
 use App\Entity\Advertisement;
+use App\Form\AdvertType;
+use Doctrine\ORM\EntityManagerInterface;
 
 class RadioController extends AbstractController
 {
+    private $em;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
     #[Route('/radio', name: 'app_home')]
     public function index(AdvertisementRepository $advertRepo): Response
     {
         $adverts = $advertRepo->findAll([], ['createdAt' => 'DESC']);
-       
         return $this->render('radio/index.html.twig', compact('adverts'));
     }
 
@@ -26,4 +35,47 @@ class RadioController extends AbstractController
     {
     	  return $this->render('advert/show.html.twig', compact("advert"));
     }
+
+    /**
+     * @Route("/adverts/create",name= "app_adverts_create", methods={"GET","POST"})
+     */
+    public function create(Request $request): Response
+    {
+        $advert = new Advertisement();
+    	$form = $this->createForm(AdvertType::class, $advert);
+    	$form->handleRequest($request);
+    	if($form->isSubmitted() && $form->isValid())
+    	{
+            $this->em->persist($advert);
+            $this->em->flush();
+            $this->addFlash('success', 'Advertisement succesfully created');
+            return $this->redirectToRoute('app_home');
+    	}
+
+    	 return $this->render('advert/create.html.twig'
+    	 	, ['form'=>$form->createView()]);
+    }
+
+
+     /**
+     * @Route("/adverts/edit/{id}", name="app_adverts_edit", requirements={"id"="\d+"}, methods={"GET","POST"})
+     */
+    public function edit(Request $request,Advertisement $advert): Response
+    {
+        $form = $this->createForm(AdvertType::class, $advert);
+
+        $form->handleRequest($request);
+        
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $this->em->flush();
+            $this->addFlash('success', 'Pin succesfully updated');
+            return $this->redirectToRoute('app_home');
+        }
+        return $this->render('advert/edit.html.twig'	, [
+            'advert'=>$advert,
+            'form'=>$form->createView()
+        ]);
+    }
+
 }
