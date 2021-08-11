7ccb094fc71c552f4ddb1918194aecc1213b2560
ruben <fopoar@gmail.com>
2021-08-11 15:30:15 -0500

    ̂ Add edit profile features

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -5,9 +5,20 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use App\Form\UserFormType;
+use Doctrine\ORM\EntityManagerInterface;
+
 
 class AccountController extends AbstractController
 {
+
+    private $em;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
     #[Route('/account', name: 'app_account')]
     public function show(): Response
     {
@@ -15,4 +26,28 @@ class AccountController extends AbstractController
            
         ]);
     }
+
+     /**
+     * @Route("/account/edit", name="app_account_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request): Response
+    {
+      
+        $user = $this->getUser();
+
+        $form = $this->createForm(UserFormType::class, $user);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+    	{
+            $this->em->flush();
+            $this->addFlash('success', 'Account successfully modified');
+        }
+
+
+        return $this->render('account/edit.html.twig'	, [
+            'form'=>$form->createView()
+        ]);
+    }
+
 }
